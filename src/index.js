1adba815ee22ac2019bc0ee61b1bfae11c1a6085
tabraha <amant@tabrahas-MacBook-Pro.local>
2021-08-26 21:25:27 -0400

    First Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList(){
+  return (<section>
+    <Book />
+    <Book />
+    <Book />
+  </section>);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
+const Title = () => <h1>Rich Dad, Poor Dad</h1>
+const Author = () => <h4>Robert T. Kiyosak</h4>
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
