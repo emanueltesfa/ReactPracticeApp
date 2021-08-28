567e658811ec76326d05ac9f5bbd521603d0b6ad
tabraha <amant@tabrahas-MacBook-Pro.local>
2021-08-28 18:38:55 -0400

    CSS and hard coded list begins

--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//format alt-shift-f
+//block comment alt-shift-a
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function BookList(){
-  return (<section>
-    <Book />
-    <Book />
-    <Book />
-  </section>);
-}
-
-const Book = () => {
+const books = [
+  {
+    title: "Rich Dad, Poor Dad",
+    author: "Robert T. Kiyosak",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    id: 1,
+  },
+  {
+    title: "The 48 Laws of Power",
+    author: "Robert Greene",
+    img: "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    id: 2,
+  },
+];
+function BookList() {
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
-    </article>
+    <section className="bookList">
+      {books.map((book) => {
+        return (
+          <Book key={book.id} /* spread js6 {...book} */ book={book}></Book>
+        );
+      })}
+    </section>
   );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
-const Title = () => <h1>Rich Dad, Poor Dad</h1>
-const Author = () => <h4>Robert T. Kiyosak</h4>
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img src={props.book.img} alt="" />
+      <h1>{props.book.title}</h1>
+      <h4>{props.book.author}</h4>
+    </article>
+  );
+};
 
-ReactDOM.render(<BookList />, document.getElementById('root'));
+ReactDOM.render(<BookList />, document.getElementById("root"));
 
+/* style={{ color: '#617d98', fontSize='0.75rem', marginTop='0.25rem' }} */
